bae4730c6a5d492c6707adfd0d92227e0c403c2b
Sarobidy <mbolasaroubidy@gmail.com>
2025-05-09 16:33:28 +0300

    feat/restrict-menu-to-my-vehicles-group

--- smart_ui_tools/models/__init__.py
+++ smart_ui_tools/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from . import vehicle_employee
 from . import fleet_vehicle_log_services
 from . import hr_employee_service_product_list_rel
+from . import vehicle_employee
 from . import v_service_product_list
+from . import res_user
